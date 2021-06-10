6d22f1bc4c263ccbb688ebf7b25e052506b73978
dasbundesland <anurag@kamalkumar.com>
2021-06-10 07:59:24 -0400

    Document and update conversion script
    Note: There is a common error in the keras2savedmodel.py script. Am working on fixing it or at least finding an adequate solution.

---
+++ util/keras2savedmodel.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import sys
+# script to convert between keras and saved model.
+# if you get an AttributeError, you might need to downgrade your h5py installation to 2.10 (https://github.com/tensorflow/tensorflow/issues/44467)
+if len(sys.argv) != 3:
+    print("Invalid arguments! See UPDATING_MODEL.txt")
+    sys.exit()
+model = tf.keras.models.load_model(sys.argv[1])
+tf.saved_model.save(model, sys.argv[2])
+
