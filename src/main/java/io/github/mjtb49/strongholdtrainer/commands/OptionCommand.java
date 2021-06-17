45a233a7cd25599c50166ebe904c018b5a9fd2be
jan-leila <jan-leila@protonmai.com>
2021-06-17 18:34:55 -0500

    fixed merge error

--- src/main/java/io/github/mjtb49/strongholdtrainer/commands/OptionCommand.java
+++ src/main/java/io/github/mjtb49/strongholdtrainer/commands/OptionCommand.java
@@ -2,15 +2,9 @@ package io.github.mjtb49.strongholdtrainer.commands;
 
 import com.google.gson.JsonElement;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import io.github.mjtb49.strongholdtrainer.StrongholdTrainer;
 import io.github.mjtb49.strongholdtrainer.util.OptionTracker;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.LiteralText;
-
-import static net.minecraft.server.command.CommandManager.argument;
-import static net.minecraft.server.command.CommandManager.literal;
 
 public abstract class OptionCommand {
     public final OptionTracker.Option optionID;
