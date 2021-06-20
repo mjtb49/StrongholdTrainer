cb681d445458074be854f23398dff050dbd7209e
dasbundesland <40721769+DasBundesland@users.noreply.github.com>
2021-06-20 18:14:03 -0400

    Prevent crashes on ML errors Closes #33

--- src/main/java/io/github/mjtb49/strongholdtrainer/ml/StrongholdMachineLearning.java
+++ src/main/java/io/github/mjtb49/strongholdtrainer/ml/StrongholdMachineLearning.java
@@ -5,6 +5,8 @@ import io.github.mjtb49.strongholdtrainer.ml.model.StrongholdModelRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 import java.util.Arrays;
 
@@ -12,7 +14,7 @@ import java.util.Arrays;
 public class StrongholdMachineLearning {
     public static final StrongholdModelRegistry MODEL_REGISTRY = new StrongholdModelRegistry();
     public static boolean verboseOutput = false;
-
+    private static final Text ERROR_MSG = new LiteralText("An error occurred querying the ML. See logs for more").formatted(Formatting.BOLD, Formatting.RED);
     public static void init(String... internalModels) {
         if(!StrongholdTrainer.ML_DISABLED){
             for(String path : internalModels){
@@ -21,23 +23,31 @@ public class StrongholdMachineLearning {
             MODEL_REGISTRY.setActiveModel("basic-classifier-nobacktracking");
         }
         // Register default included models.
-
     }
+    private static final double[] ERROR_POLICY = new double[]{0xFFD, 0xFFD, 0xFFD, 0xFFD, 0xFFD, 0xFFD};
 
     public static double[] getPredictions(StrongholdPath path) {
         //hack fix since the model hasn't been trained on rooms where the portal room is adjacent
-        if((!StrongholdTrainer.ML_DISABLED) || (MODEL_REGISTRY.getActiveModel() != null)){
-            double[] predictions = MODEL_REGISTRY.getActiveModel().getPredictions(path);
-            if(verboseOutput){
-                PlayerEntity playerEntity = MinecraftClient.getInstance().player;
-                if(playerEntity != null){
-                    playerEntity.sendMessage(new LiteralText(Arrays.toString(predictions)), false);
+        try{
+            if((!StrongholdTrainer.ML_DISABLED) || (MODEL_REGISTRY.getActiveModel() != null)){
+                double[] predictions = MODEL_REGISTRY.getActiveModel().getPredictions(path);
+                if(verboseOutput){
+                    PlayerEntity playerEntity = MinecraftClient.getInstance().player;
+                    if(playerEntity != null){
+                        playerEntity.sendMessage(new LiteralText(Arrays.toString(predictions)), false);
+                    }
                 }
+                return MODEL_REGISTRY.getActiveModel().getPredictions(path);
+            } else {
+                return ERROR_POLICY;
             }
-            return MODEL_REGISTRY.getActiveModel().getPredictions(path);
-        } else {
-            return new double[]{0xffD, 0xffD, 0xffD, 0xffD, 0xffD};
+        } catch (Exception e){
+            if(MinecraftClient.getInstance().player != null){
+                MinecraftClient.getInstance().player.sendMessage(ERROR_MSG, false);
+            }
+            return ERROR_POLICY;
         }
+
     }
 
 
