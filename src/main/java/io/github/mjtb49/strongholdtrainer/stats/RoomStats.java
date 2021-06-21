b78b1e7c38f524d32a81148f5de766e42d90c9bf
Matthew Bolan <mjb6117@rit.edu>
2021-06-21 13:27:50 -0400

    Removed some useless room statistics.

--- src/main/java/io/github/mjtb49/strongholdtrainer/stats/RoomStats.java
+++ src/main/java/io/github/mjtb49/strongholdtrainer/stats/RoomStats.java
@@ -16,35 +16,42 @@ public class RoomStats {
     private static final Map<Class<? extends StructurePiece>, Identifier> roomTimeStats = new HashMap<>();
     private static final Map<Class<? extends StructurePiece>, Identifier> averageTimeStats = new HashMap<>();
 
+    private static boolean isValid(Class<? extends StructurePiece> pieceType) {
+        return ((!RoomFormatter.ROOM_TO_STRING.get(pieceType).equals("PortalRoom")) && (!RoomFormatter.ROOM_TO_STRING.get(pieceType).equals("None")));
+    }
+
     public static void register() {
         for (Class<? extends StructurePiece> pieceType : RoomFormatter.ROOM_TO_STRING.keySet()) {
-            String countName = "piece_" + RoomFormatter.ROOM_TO_STRING.get(pieceType).toLowerCase().replace(" ","_") + "_count";
-            String timeName =  "piece_" + RoomFormatter.ROOM_TO_STRING.get(pieceType).toLowerCase().replace(" ","_") + "_total";
-            String avgName =   "piece_" + RoomFormatter.ROOM_TO_STRING.get(pieceType).toLowerCase().replace(" ","_") + "_avg";
+            if (isValid(pieceType)) {
+                String countName = "piece_" + RoomFormatter.ROOM_TO_STRING.get(pieceType).toLowerCase().replace(" ", "_") + "_count";
+                String timeName = "piece_" + RoomFormatter.ROOM_TO_STRING.get(pieceType).toLowerCase().replace(" ", "_") + "_total";
+                String avgName = "piece_" + RoomFormatter.ROOM_TO_STRING.get(pieceType).toLowerCase().replace(" ", "_") + "_avg";
 
-            roomCountStats.put(pieceType, new Identifier(StrongholdTrainerStats.MODID, countName));
-            roomTimeStats.put(pieceType, new Identifier(StrongholdTrainerStats.MODID, timeName));
-            averageTimeStats.put(pieceType, new Identifier(StrongholdTrainerStats.MODID, avgName));
+                roomCountStats.put(pieceType, new Identifier(StrongholdTrainerStats.MODID, countName));
+                roomTimeStats.put(pieceType, new Identifier(StrongholdTrainerStats.MODID, timeName));
+                averageTimeStats.put(pieceType, new Identifier(StrongholdTrainerStats.MODID, avgName));
 
-            Registry.register(Registry.CUSTOM_STAT, countName, roomCountStats.get(pieceType));
-            Registry.register(Registry.CUSTOM_STAT, timeName, roomTimeStats.get(pieceType));
-            Registry.register(Registry.CUSTOM_STAT, avgName, averageTimeStats.get(pieceType));
+                Registry.register(Registry.CUSTOM_STAT, countName, roomCountStats.get(pieceType));
+                Registry.register(Registry.CUSTOM_STAT, timeName, roomTimeStats.get(pieceType));
+                Registry.register(Registry.CUSTOM_STAT, avgName, averageTimeStats.get(pieceType));
 
-            Stats.CUSTOM.getOrCreateStat(roomCountStats.get(pieceType), StatFormatter.DEFAULT);
-            Stats.CUSTOM.getOrCreateStat(roomTimeStats.get(pieceType), StatFormatter.TIME);
-            Stats.CUSTOM.getOrCreateStat(averageTimeStats.get(pieceType), StatFormatter.TIME);
+                Stats.CUSTOM.getOrCreateStat(roomCountStats.get(pieceType), StatFormatter.DEFAULT);
+                Stats.CUSTOM.getOrCreateStat(roomTimeStats.get(pieceType), StatFormatter.TIME);
+                Stats.CUSTOM.getOrCreateStat(averageTimeStats.get(pieceType), StatFormatter.TIME);
+            }
         }
     }
 
     public static void updateRoomStats(ServerPlayerEntity playerEntity, Class<? extends StructurePiece> pieceType, int timeInTicks) {
         //because of this increment count should never be 0
-        playerEntity.incrementStat(roomCountStats.get(pieceType));
-        playerEntity.increaseStat(roomTimeStats.get(pieceType), timeInTicks);
-        int count = playerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(roomCountStats.get(pieceType)));
-        int time = playerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(roomTimeStats.get(pieceType)));
-        int avg = time / count;
-        playerEntity.resetStat(Stats.CUSTOM.getOrCreateStat(averageTimeStats.get(pieceType)));
-        playerEntity.increaseStat(averageTimeStats.get(pieceType), avg);
+        if (isValid(pieceType)) {
+            playerEntity.incrementStat(roomCountStats.get(pieceType));
+            playerEntity.increaseStat(roomTimeStats.get(pieceType), timeInTicks);
+            int count = playerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(roomCountStats.get(pieceType)));
+            int time = playerEntity.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(roomTimeStats.get(pieceType)));
+            int avg = time / count;
+            playerEntity.resetStat(Stats.CUSTOM.getOrCreateStat(averageTimeStats.get(pieceType)));
+            playerEntity.increaseStat(averageTimeStats.get(pieceType), avg);
+        }
     }
-
 }
