ac08936a5d1dfa90dfbdcd31f6ea86892bd14c4f
dasbundesland <40721769+DasBundesland@users.noreply.github.com>
2021-09-04 10:31:31 -0400

    Clean up util directory

--- util/keras2savedmodel.py
+++ util/keras2savedmodel.py
@@ -3,7 +3,7 @@ import sys
 # script to convert between keras and saved model.
 # if you get an AttributeError, you might need to downgrade your h5py installation to 2.10 (https://github.com/tensorflow/tensorflow/issues/44467)
 if len(sys.argv) != 3:
-    print("Invalid arguments! See UPDATING_MODEL.txt")
+    print("Invalid arguments! <.keras file> <directory>")
     sys.exit()
 model = tf.keras.models.load_model(sys.argv[1])
 tf.saved_model.save(model, sys.argv[2])
